16912918632fd5cfee83aa542c1d5caa88722418
Rashmi <rashmijayasekara00@gmail.com>
2022-11-29 13:53:46 +0530

    Create ToDoItemDTO

---
+++ src/main/java/lk/ijse/dep9/dto/ToDoItemDTO.java
@@ -0,0 +1,10 @@
+package lk.ijse.dep9.dto;
+
+import java.io.Serializable;
+
+public class ToDoItemDTO implements Serializable {
+    private Integer id;
+    private String username;
+    private String description;
+    private String status;
+}
