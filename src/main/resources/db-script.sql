1d27afe81849b0b4b2881cfe56ca4ae411aa27a5
Rashmi <rashmijayasekara00@gmail.com>
2022-11-29 11:41:58 +0530

    create todo-item table

--- src/main/resources/db-script.sql
+++ src/main/resources/db-script.sql
@@ -3,3 +3,11 @@ CREATE TABLE user(
     password VARCHAR(100) NOT NULL ,
     fullName VARCHAR(200) NOT NULL
 );
+
+CREATE TABLE todo_item(
+  id INT AUTO_INCREMENT PRIMARY KEY ,
+  username VARCHAR(40) NOT NULL ,
+  description VARCHAR(200) NOT NULL ,
+  status ENUM('DONE','NOT-DONE') NOT NULL ,
+  CONSTRAINT fk FOREIGN KEY (username) REFERENCES user(username)
+);
