dbb8f6542669ccdf07fa943904b565f2e8517ee1
Nipuni-Jayathilaka <nipunipavithrajayathilaka@gmail.com>
2022-11-29 11:58:38 +0530

    Initiate dao layer

---
+++ src/main/java/lk/ijse/dep9/dao/custom/UserDAO.java
@@ -0,0 +1,7 @@
+package lk.ijse.dep9.dao.custom;
+
+public interface UserDAO {
+    void createUser();
+    void deleteUser();
+
+}
---
+++ src/main/java/lk/ijse/dep9/entity/User.java
@@ -0,0 +1,6 @@
+package lk.ijse.dep9.entity;
+
+public class User {
+
+}
+
